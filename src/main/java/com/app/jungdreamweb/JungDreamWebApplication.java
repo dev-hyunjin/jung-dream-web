cffd33405671fa7d0b8ae57509a19b7edb3276d5
dev-hyunjin <disappointed123419@gmail.com>
2024-09-12 10:16:26 +0900

    프로젝트 생성

---
+++ src/main/java/com/app/jungdreamweb/JungDreamWebApplication.java
@@ -0,0 +1,13 @@
+package com.app.jungdreamweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JungDreamWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JungDreamWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/jungdreamweb/JungDreamWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.jungdreamweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JungDreamWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
